cdf66b06303c6c337014100a6f090b3343cdd30a
MichaelD <enee.kors@mail.ru>
2021-10-11 11:21:42 +0300

    Initial commit

---
+++ car.js
@@ -0,0 +1,8 @@
+const carName = 'Audi'
+let maxCarSpeed = '300 km/h'
+let carOwner = 'Yana'
+
+maxCarSpeed = '250 km/h'
+carOwner = 'Misha'
+console.log('У машины изменилась скорость', maxCarSpeed)
+console.log('У машины изменился владелец', carOwner)
